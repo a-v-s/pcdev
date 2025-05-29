c820c5993101bbece4fc2208f5a72b699bf6d9a9
André van Schoubroeck <andre@blaatschaap.be>
2025-05-29 13:20:23 +0200

    {C,CXX}FLAGS per compiler version

--- build/make/cc.mk
+++ build/make/cc.mk
@@ -1,36 +1,102 @@
-
 ifeq ($(COMPILER),gcc) 
 	CC=$(PREFIX)gcc
 	CXX=$(PREFIX)g++
-#	LINKCXX=$(CXX)
-#	LINKC=$(CC)
 	AR=$(PREFIX)ar
 	AS=$(PREFIX)as
 else
 ifeq ($(COMPILER),clang) 
 	CC=clang
 	CXX=clang++
-#	LINKCXX=$(CXX)
-#	LINKC=$(CC)
 	AR=llvm-ar
 	AS=llvm-as
 endif
 endif
 
+# Get the compiler version
+CC_VERSION       := $(shell $(CC) -dumpversion)
+# Replaqce dot with space
+CC_VERSION_SPACE := $(subst ., ,$(CC_VERSION))
+# Split on space, get first, second, third word
+CC_VERSION_MAJOR := $(word 1,$(CC_VERSION_SPACE))
+CC_VERSION_MINOR := $(word 2,$(CC_VERSION_SPACE))
+CC_VERSION_PATCH := $(word 3,$(CC_VERSION_SPACE))
+
+$(info Compiler: $(COMPILER) version $(CC_VERSION_MAJOR))
 
 
-#CXXFLAGS += -Werror
+ifeq ($(COMPILER),gcc) 
+	# Select the greatest C version for the gcc version
+	ifeq ($(shell test $(CC_VERSION_MAJOR) -ge 14; echo $$?),0)
+		CFLAGS   += --std=gnu23
+	else
+	ifeq ($(shell test $(CC_VERSION_MAJOR) -ge 8; echo $$?),0)
+		CFLAGS += --std=gnu17
+	else
+	ifeq ($(shell test $(CC_VERSION_MAJOR) -ge 5; echo $$?),0)
+		CFLAGS += --std=gnu11
+	else
+		CFLAGS += --std=gnu99
+	endif
+	endif
+	endif
+
+	# Select the greatest C++ version for the gcc version
+	ifeq ($(shell test $(CC_VERSION_MAJOR) -ge 14; echo $$?),0)
+		CXXFLAGS += --std=gnu++26
+	else
+	ifeq ($(shell test $(CC_VERSION_MAJOR) -ge 11; echo $$?),0)
+		CXXFLAGS += --std=gnu++23
+	ifeq ($(shell test $(CC_VERSION_MAJOR) -ge 10; echo $$?),0)
+		CXXFLAGS += --std=gnu++20
+	else
+	ifeq ($(shell test $(CC_VERSION_MAJOR) -ge 5; echo $$?),0)
+		CXXFLAGS += --std=gnu++17
+	else
+		CXXFLAGS += --std=gnu++11
+	endif
+	endif
+	endif
+	endif
+else
+ifeq ($(COMPILER),clang) 
+	# Select the greatest C version for the clang version
+	ifeq ($(shell test $(CC_VERSION_MAJOR) -gt 18; echo $$?),0)
+		CFLAGS += --std=gnu23
+	else 
+	ifeq ($(shell test $(CC_VERSION_MAJOR) -gt 6; echo $$?),0)
+		CFLAGS += --std=gnu17
+	else 
+	ifeq ($(shell test $(CC_VERSION_MAJOR) -gt 4; echo $$?),0)
+		CFLAGS += --std=gnu11
+	else
+		CFLAGS += --std=gnu99
+	endif
+	endif
+	endif
+
+	# Select the greatest C++ version for the clang version
+	ifeq ($(shell test $(CC_VERSION_MAJOR) -ge 17; echo $$?),0)
+		CXXFLAGS += --std=gnu++26
+	else
+	ifeq ($(shell test $(CC_VERSION_MAJOR) -ge 12; echo $$?),0)
+		CXXFLAGS += --std=gnu++2b
+	else
+	ifeq ($(shell test $(CC_VERSION_MAJOR) -ge 10; echo $$?),0)
+		CXXFLAGS += --std=gnu++20
+	else
+	ifeq ($(shell test $(CC_VERSION_MAJOR) -ge 5; echo $$?),0)
+		CXXFLAGS += --std=gnu++17
+	else
+		CXXFLAGS += --std=gnu++11
+	endif
+	endif
+	endif
+	endif
+endif
+endif
+
 CXXFLAGS += -Wall
-
-# For GCC, gnu++23 and gnu++2b are equivalent, but clang only accepts 2b
-CXXFLAGS += --std=gnu++23
-#CXXFLAGS += --std=gnu++2b
-#CXXFLAGS += --std=gnu++26 # starting gcc 14
-
-#CFLAGS += -Werror
 CFLAGS += -Wall
-#CFLAGS += --std=gnu2x
-CFLAGS += --std=gnu23
 
 ifeq ($(CXX_SRC),)
 	LINK := $(CC)
